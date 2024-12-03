750d2f9a465c3de2ffa2bdd2e1706ea11bd5ad5a
Moriel Mauni <moriel.ma0@gmail.com>
2024-12-04 00:26:09 +0200

    Create workflow.tf

---
+++ workflow.tf
@@ -0,0 +1,25 @@
+############################### *! Auto Format Code ##############################
+
+?   terraform fmt
+
+############################### *! Taint and Replace ##############################
+
+?   terraform apply -replace = "name"
+
+############################### *! Import ##############################
+
+?   terraform import resource_type.resource_name.id
+
+*/
+
+
+
+/*
+
+############################### *! Workspace- OSS ##############################
+
+? terraform show
+? terraform new <name>
+? terraform list
+? terraform select <name>
+? terraform delete
